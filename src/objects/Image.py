8881b3b157db325e549415d1576098138d704537
roscas <sol.rosca@gmail.com>
2020-05-14 00:32:25 +0200

    lazy loading for Image class

--- src/objects/Image.py
+++ src/objects/Image.py
@@ -17,6 +17,12 @@ from src.objects.Point import Point
 
 
 class Image:
+	"""
+	Representation of an image.
+	Contain usefull processing methodes used around the program
+	and some abstractions to make it easy to use with cv2.
+	"""
+
 	RHO = 1
 	THETA = np.pi / 180
 	THRESHOLD = 80
@@ -24,30 +30,40 @@ class Image:
 	MAX_LINE_LENGTH = 150
 
 	def __init__(self, pixels):
-		self.pixels = pixels
+		self._gray = None
+		self._gaussian = None
+		self._canny = None
+		self._edges = None
+		self._lines = None
 
-	# IMPLEMENT LAZY
+		self.pixels = pixels
 
 	@property
 	def np(self) -> np.ndarray:
 		return cv2.cvtColor(self.pixels, cv2.COLOR_BGR2RGB)
 
 	def gray(self) -> object:
-		return Image(cv2.cvtColor(self.pixels, cv2.COLOR_BGR2GRAY))
+		if self._gray is None:
+			self._gray = Image(cv2.cvtColor(self.pixels, cv2.COLOR_BGR2GRAY))
+		return self._gray
 
 	def gaussian_blur(self, kernel: Tuple[int] = (5, 5)) -> object:
-		return Image(cv2.GaussianBlur(self.pixels, kernel, 0))
+		if self._gaussian is None:
+			self._gaussian = Image(cv2.GaussianBlur(self.pixels, kernel, 0))
+		return self._gaussian
 
 	def canny(self, low_bound: int = 80, high_bound: int = 110) -> object:
-		return Image(
-			cv2.Canny(self.pixels, threshold1=low_bound,
-					  threshold2=high_bound))
+		if self._canny is None:
+			self._canny = Image(cv2.Canny(self.pixels, threshold1=low_bound, threshold2=high_bound))
+		return self._canny
 
 	def edges(self) -> object:
-		gray = self.gray()
-		blurred = gray.gaussian_blur()
-		canny = blurred.canny()
-		return canny
+		if self._edges is None:
+			gray = self.gray()
+			blurred = gray.gaussian_blur()
+			canny = blurred.canny()
+			self._edges = canny
+		return self._edges
 
 	def find_lines(self) -> List[Line] or None:
 		raw = cv2.HoughLinesP(self.pixels,
